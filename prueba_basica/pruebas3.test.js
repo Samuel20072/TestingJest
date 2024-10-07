503bef12c050511b4c03fff24a6a3009c5f109f3
kelvinocampo <145300425+kelvinocampo@users.noreply.github.com>
2024-10-07 10:35:05 -0500

    add prueba3 test2 test3

--- prueba_basica/pruebas3.test.js
+++ prueba_basica/pruebas3.test.js
@@ -10,3 +10,24 @@ test("Null y variable definida", () => {
     expect(n).toBeFalsy(); // asegura que sea falso en valor booleano
     // Ejemplos de falso en booleano: false, "", 0, null, undefined, NaN
 });
+
+test('2 + 2', () => {
+    let suma = 2 + 2;
+    expect(suma).toBeGreaterThan(3); // Mayor a 3
+    expect(suma).toBeGreaterThanOrEqual(3.5); // Mayor o igual a 3.5
+    expect(suma).toBeLessThan(5); // Menor a 5
+    expect(suma).toBeLessThanOrEqual(4.5); // Menor o igual
+
+    // toBe y toEqual son equivalentes para números
+    expect(suma).toBe(4);
+    expect(suma).toEqual(4);
+});
+
+test('Valor flotante o decimal', () => {
+    const valor = 0.1 + 0.2;
+    // expect(value).toBe(0.3); Esto no funcionará debido al error de redondeo
+
+    /* Debido a cómo se representan internamente los números de punto flotante, 
+    el resultado no es exactamente 0.3, sino algo como 0.30000000000000004. */
+    expect(valor).toBeCloseTo(0.3); // Esto funciona.
+});
